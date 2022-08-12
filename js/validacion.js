463027e9937250a2c8d2d1175f6d87d61b7b3c34
maglisg <magyg16@gmail.com>
2022-08-12 20:05:49 -0300

    Update validacion.js
    ningún cambio

--- js/validacion.js
+++ js/validacion.js
@@ -1,9 +1,3 @@
-let nombre= document.getElementById("nombre").value
-let apellido= document.getElementById("apellido").value
-let email= document.getElementById("email").value
-let password= document.getElementById("password").value
-let password1= document.getElementById("password1").value
-
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
