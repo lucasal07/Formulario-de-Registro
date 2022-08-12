c7d0806804719b5f39643e3fcbab5a5f49cd01db
juansanpedro <juanignacio50@hotmail.com>
2022-08-12 19:29:49 -0300

    commit juan

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,9 @@
+let nombre= document.getElementById("nombre").value
+let apellido= document.getElementById("apellido").value
+let email= document.getElementById("email").value
+let password= document.getElementById("password").value
+let password1= document.getElementById("password1").value
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -5,3 +11,20 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function campoVacio(){
+
+}
+
+function passwordMin(){
+
+}
+
+function passwordRepetir(){
+
+}
+
+function passwordRepetir(){
+
+}
+
