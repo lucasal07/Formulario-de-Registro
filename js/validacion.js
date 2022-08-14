bb774dc14ff64e8e3209cb1ee39313dc3bacbf64
maglisg <magyg16@gmail.com>
2022-08-14 18:52:10 -0300

    funciona si hacen todo correcto a la primera, si corrigen algo y vuelven a darle registrar sale un error en la función que mando el profe.

--- js/validacion.js
+++ js/validacion.js
@@ -6,19 +6,28 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-function campoVacio(){
 
-}
+function requirement () {
+    var nombre = document.getElementById("nombre").value
+    var apellido = document.getElementById("apellido").value
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password1").value
+    var password1 = document.getElementById("password2").value
 
-function passwordMin(){
+    if ((nombre.length  || apellido.length || email.length) == 0 ){
+        showAlertError()
+    } else if (password.length <= 5){
+        showAlertError()
+    } else if (password !== password1) {
+        showAlertError()
+    }
+    else {
+       showAlertSuccess()
+    }
+}  
 
-}
+const registro = document.getElementById("regBtn") 
+registro.addEventListener ( "click", requirement)
 
-function passwordRepetir(){
 
-}
-
-function passwordRepetir(){
-
-}
 
