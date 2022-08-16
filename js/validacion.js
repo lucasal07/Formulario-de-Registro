fd2c8773c7875bbf538a7f7988124e7c08a84e6f
juansanpedro <juanignacio50@hotmail.com>
2022-08-15 21:20:55 -0300

    .

--- js/validacion.js
+++ js/validacion.js
@@ -1,9 +1,3 @@
-let nombre= document.getElementById("nombre").value
-let apellido= document.getElementById("apellido").value
-let email= document.getElementById("email").value
-let password= document.getElementById("password").value
-let password1= document.getElementById("password1").value
-
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -12,19 +6,24 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-function campoVacio(){
+function requirement () {
+    var nombre = document.getElementById("nombre").value
+    var apellido = document.getElementById("apellido").value
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password1").value
+    var password1 = document.getElementById("password2").value
 
-}
+    if ((nombre.length  || apellido.length || email.length) == 0 ){
+        showAlertError()
+    } else if (password.length <= 5){
+        showAlertError()
+    } else if (password !== password1) {
+        showAlertError()
+    }
+    else {
+       showAlertSuccess()
+    }
+}  
 
-function passwordMin(){
-
-}
-
-function passwordRepetir(){
-
-}
-
-function passwordRepetir(){
-
-}
-
+const registro = document.getElementById("regBtn") 
+registro.addEventListener ( "click", requirement)
